aa81e13b4b5a0e355ceec92620f6f5826ab7bd46
mir-olga <olga.mirzoeva21@gmail.com>
2024-07-11 23:37:50 +0300

    all

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,28 @@
-import { FaUserAlt } from "react-icons/fa";
-import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaUserAlt } from 'react-icons/fa';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 import css from './Contact.module.css';
-import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 
-export const Contact = ({item}) => {
-    const dispatch = useDispatch ();
+export const Contact = ({ item }) => {
+  const dispatch = useDispatch();
 
-    return (
-      <div className={css.contact}>
-        <div className={css.info}>
-          <p className={css.name}>
-            <FaUserAlt /> {item.name}
-          </p>
-          <p className={css.name}>
-            {' '}
-            <BsFillTelephoneFill /> {item.number}
-          </p>
-        </div>
-        <div>
-          <button className={css.button} onClick={() => dispatch(deleteContact(item.id))}>
-            Delete
-          </button>
-        </div>
+  return (
+    <div className={css.contact}>
+      <div className={css.info}>
+        <p className={css.name}>
+          <FaUserAlt /> {item.name}
+        </p>
+        <p className={css.name}>
+          {' '}
+          <BsFillTelephoneFill /> {item.number}
+        </p>
       </div>
-    );
-}
+      <div>
+        <button className={css.button} onClick={() => dispatch(deleteContact(item.id))}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
